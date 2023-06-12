1c1c87d76ec54dfce738d03234211ec1bf971d2c
Pavel Veselev <veselev@gmail.com>
2023-06-12 16:03:55 +0300

    Batch delay

--- Consumer/Consumers/TestBatchConsumer.cs
+++ Consumer/Consumers/TestBatchConsumer.cs
@@ -8,5 +8,9 @@ public class TestBatchConsumer : IConsumer<Batch<TestBatchMessage>>
     public async Task Consume(ConsumeContext<Batch<TestBatchMessage>> context)
     {
         Console.WriteLine($"Messages count: {context.Message.Count()}. Message ids: [{string.Join(",", context.Message.Select(x => x.Message.Id))}]");
+
+        await Task.Delay(TimeSpan.FromSeconds(3));
+
+        Console.WriteLine($"Messages count: {context.Message.Count()}. Message ids: [{string.Join(",", context.Message.Select(x => x.Message.Id))}]. Handled.");
     }
 }
